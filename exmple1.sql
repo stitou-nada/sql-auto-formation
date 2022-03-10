1e3c51b02d3a19b37326cf4cef14c520095e318b
stitou-nada <nadastito1087@gmail.com>
2022-03-10 12:51:53 +0000

    test

--- exmple1.sql
+++ exmple1.sql
@@ -1,9 +1,16 @@
+-- cree un base de donné
 CREATE DATABASE `student`;
-SELECT * FROM `stagiaire`
-SELECT nom FROM `stagiare`
+-- entrer dans base de donné
 USE student;
+-- cree une table
 CREATE TABLE stagiaire (
  id int PRIMARY KEY ,
  nom varchar (250)
 );
+-- insert dans le tableaux
 INSERT INTO `stagiaire` (`id`, `nom`) VALUES ('1','nada');
+-- selectionne tableaux
+SELECT * FROM `stagiaire`
+SELECT nom FROM `stagiare`
+
+
