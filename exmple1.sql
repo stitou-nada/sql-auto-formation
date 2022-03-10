95141074ebc38451b5585c05a6a8ff0993e7e05e
stitou-nada <nadastito1087@gmail.com>
2022-03-10 12:46:29 +0000

    sql

---
+++ exmple1.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `student`;
+SELECT * FROM `stagiaire`
+SELECT nom FROM `stagiare`
+USE student;
+CREATE TABLE stagiaire (
+ id int PRIMARY KEY ,
+ nom varchar (250)
+);
+INSERT INTO `stagiaire` (`id`, `nom`) VALUES ('1','nada');
